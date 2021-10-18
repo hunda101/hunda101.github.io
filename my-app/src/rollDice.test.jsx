a3ec48fb10975ce648a7f5e32aaa67d8a5bdbf3b
Mark Khomenko <2markkhomenko@gmail.com>
2021-10-18 10:30:52 +0300

    Updates

--- my-app/src/rollDice.test.jsx
+++
@@ -1,6 +0,0 @@
-import {rollDice} from './rollDice';
-
-it('rollDice generates random numbers from 1 to 6', () => {
-    const result = rollDice();
-    expect(result[0] >= 1 && result[0] <= 6).toEqual(true);
-  });
